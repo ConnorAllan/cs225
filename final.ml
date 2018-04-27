d1cb151f852ecee63a01da920809c586c5063111
ConnorAllan <Connor.Allan@uvm.edu>
2018-04-27 17:19:17 -0400

    Update final.ml

--- final.ml
+++ final.ml
@@ -54,6 +54,12 @@ type value =
   | VLoc of loc
 [@@deriving show {with_path = false}]
 
+let rec exp_of_val (v : value) : exp = match v with
+  | VTrue -> True
+  | VFalse -> False
+  | VPair(v1,v2) -> Pair(exp_of_val v1,exp_of_val v2)
+  | VLoc(l) -> Loc(l)
+
 type store = (loc * value) list
 [@@deriving show {with_path = false}]
 
@@ -65,104 +71,35 @@ type result =
   | Stuck
 [@@deriving show {with_path = false}]
 
-(* Typing relation encoded as an inference metafunction. *)
-let rec step (e0 : exp) : result = match e0 with
-  (* true ∈ val *)
+let rec step (e0 : exp) (s : store) : result = match e0 with
   | True -> Val(VTrue)
-  (* false ∈ val *)
   | False -> Val(VFalse)
-  | If(e1,e2,e3) -> begin match step e1 with
-      (* [If-True]
-       * if(true){e₂}{e₃} —→ e₂ *)
-      | Val(VTrue) -> Step(e2)
-      (* [If-False]
-       * if(false){e₂}{e₃} —→ e₃ *)
-      | Val(VFalse) -> Step(e3)
-      (* v ∉ {true,false}
-       * ⟹
-       * if(v){e₂}{e₃} ∉ val
-       * if(v){e₂}{e₃} —↛ *)
+  | If(e1,e2,e3) -> begin match step e1 s with
+      | Val(VTrue) -> Step(e2,s)
+      | Val(VFalse) -> Step(e3,s)
       | Val(_) -> Stuck
-      (* [If-Cong]
-       * e₁ —→ e₁′
-       * ⟹
-       * if(e₁){e₂}{e₃} —→ if(e₁′){e₂}{e₃} *)
-      | Step(e1') -> Step(If(e1',e2,e3))
-      (* e₁ ∉ val
-       * e₁ —↛
-       * ⟹
-       * if(e₁){e₂}{e₃} ∉ val
-       * if(e₁){e₂}{e₃} —↛ *)
+      | Step(e1',s') -> Step(If(e1',e2,e3),s')
       | Stuck -> Stuck
       end
-  | Pair(e1,e2) -> begin match step e1 with
-      | Val(v1) -> begin match step e2 with
-          (* ⟨v₁,v₂⟩ ∈ val *)
+  | Pair(e1,e2) -> begin match step e1 s with
+      | Val(v1) -> begin match step e2 s with
           | Val(v2) -> Val(VPair(v1,v2))
-          (* [Pair-Cong-2]
-           * e —→ e′
-           * ⟹
-           * ⟨v,e⟩ —→ ⟨v,e′⟩ *)
-          | Step(e2') -> Step(Pair(e1,e2'))
-          (* e ∉ val
-           * e —↛
-           * ⟹
-           * ⟨v,e⟩ ∉ val
-           * ⟨v,e⟩ —↛ *)
+          | Step(e2',s') -> Step(Pair(e1,e2'),s')
           | Stuck -> Stuck
           end
-      (* [Pair-Cong-1]
-       * e₁ —→ e₁′
-       * ⟹
-       * ⟨e₁,e₂⟩ —→ ⟨e₁′,e₂⟩ *)
-      | Step(e1') -> Step(Pair(e1',e2))
-      (* e₁ ∉ val
-       * e₁ —↛
-       * ⟹
-       * ⟨e₁,e₂⟩ ∉ val
-       * ⟨e₁,e₂⟩ —↛ *)
+      | Step(e1',s') -> Step(Pair(e1',e2),s')
       | Stuck -> Stuck
       end
-  | Projl(e1) -> begin match step e1 with
-      (* [Projl-Pair]
-       * projl(⟨v₁,v₂⟩) —→ v₁ *)
-      | Val(VPair(v1,v2)) -> Step(exp_of_val v1)
-      (* ∄v₁,v₂. v = ⟨v₁,v₂⟩
-       * ⟹
-       * projl(v) ∉ val
-       * projl(v) —↛ *)
-      | Val(_) -> Stuck
-      (* [Projl-Cong]
-       * e —→ e′
-       * ⟹
-       * projl(e) —→ projl(e′) *)
-      | Step(e1') -> Step(Projl(e1'))
-      (* e ∉ val
-       * e —↛
-       * ⟹
-       * projl(e) ∉ val
-       * projl(e) —↛ *)
+  | Projl(e1) -> begin match step e1 s with
+      | Val(VPair(v1,v2)) -> Step(exp_of_val v1,s)
+      | Val(_) -> Stuck                                                      
+      | Step(e1',s') -> Step(Projl(e1'),s')
       | Stuck -> Stuck
       end
-  | Projr(e1) -> begin match step e1 with
-      (* [Projr-Pair]
-       * projr(⟨v₁,v₂⟩) —→ v₂ *)
-      | Val(VPair(v1,v2)) -> Step(exp_of_val v2)
-      (* ∄v₁,v₂. v = ⟨v₁,v₂⟩
-       * ⟹
-       * projr(v) ∉ val
-       * projr(v) —↛ *)
+  | Projr(e1) -> begin match step e1 s with
+      | Val(VPair(v1,v2)) -> Step(exp_of_val v2,s)
       | Val(_) -> Stuck
-      (* [Projr-Cong]
-       * e —→ e′
-       * ⟹
-       * projr(e) —→ projr(e′) *)
-      | Step(e1') -> Step(Projr(e1'))
-      (* e ∉ val
-       * e —↛
-       * ⟹
-       * projr(e) ∉ val
-       * projr(e) —↛ *)
+      | Step(e1',s') -> Step(Projr(e1'),s')
       | Stuck -> Stuck
       end
       | Loc(l) -> Val(VLoc(l))
